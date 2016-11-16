32c1114ec65e8eeb7258249f9c6809023805e8ba
Carson Evans <cevans@opentext.com>
2016-11-16 15:41:29 -0500

    Added date to logs

--- .scripts/bing-wp-download.sh
+++ .scripts/bing-wp-download.sh
@@ -10,13 +10,14 @@ urls=( $(curl -sL http://www.bing.com | \
     sed -e "s/\\\//g") )
 
 for p in ${urls[@]}; do
+    echo -n $(date +"%F %T")
     filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
     if [ ! -f "$PICTURE_DIR/$filename.png" ]; then
-        echo "Downloading: $filename ..."
+        echo " Downloading: $filename ..."
         curl -Lo "$PICTURE_DIR/$filename" $p
         convert "$PICTURE_DIR/$filename" "$PICTURE_DIR/$filename.png"
         rm "$PICTURE_DIR/$filename"
     else
-        echo "Skipping: $filename ..."
+        echo " Skipping: $filename ..."
     fi
 done
