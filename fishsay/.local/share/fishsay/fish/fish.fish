5582a47aed5484026625c255baba5be4452b5bc3
Carson Evans <carsonwevans@gmail.com>
2026-04-19 16:56:00 -0400

    created cowsay inspired function fishsay

---
+++ fishsay/.local/share/fishsay/fish/fish.fish
@@ -0,0 +1,13 @@
+##
+## Default fish creature for fishsay.
+##
+## The {thoughts} placeholder is replaced with the speech bubble connector.
+## Use \\ for a literal backslash and \" for a literal double-quote in the art.
+##
+set the_fish "     {thoughts}
+      {thoughts}    /|
+         _/ |
+      ,-´    `-:..-´/
+     : o )      _  (
+     \"`-. ..,--; `-.\\
+         `'"
