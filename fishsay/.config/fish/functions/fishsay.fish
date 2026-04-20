44eccc3393da17258d379e26710ef1dee4fc5c22
Carson Evans <carsonwevans@gmail.com>
2026-04-19 20:39:42 -0400

    add fishthink, and convert fish creatures to plain text to avoid escaping

--- fishsay/.config/fish/functions/fishsay.fish
+++ fishsay/.config/fish/functions/fishsay.fish
@@ -1,32 +1,33 @@
 #
 # A cowsay-inspired ASCII art speech bubble generator, powered by fish.
-# Creature art is defined in ~/.local/share/fishsay/fish/<name>.fish files,
-# each setting a $the_fish variable with {thoughts} as the connector
-# placeholder.
+# Creature art is defined in ~/.local/share/fishsay/fish/<name>.txt files,
+# plain text with {thoughts} as the connector placeholder. No escaping needed.
 #
-# Usage: fishsay [-f <name>] [-W <width>] [-n] [-r] [-l] [-h] [message]
+# Usage: fishsay [-f <name>] [-W <width>] [-n] [-r] [-t] [-l] [-h] [message]
 #   If no message is given, reads from stdin.
 #
 # Options:
 #   -f, --file <name>  Use a named creature (default: fish)
 #   -r, --random           Pick a random creature
+#   -t, --think            Use a thought bubble instead of a speech bubble
 #   -W, --wrap <width>     Wrap message at <width> characters (default: 40)
 #   -n, --nowrap           Disable word wrapping
 #   -l, --list             List available creatures
 #   -h, --help             Show this help message
 #
 function fishsay
-    argparse 'h/help' 'f/file=' 'r/random' 'W/wrap=' 'n/nowrap' 'l/list' -- $argv
+    argparse 'h/help' 'f/file=' 'r/random' 't/think' 'W/wrap=' 'n/nowrap' 'l/list' -- $argv
     or return 1
 
     if set -q _flag_help
-        echo "Usage: fishsay [-f <name>] [-W <width>] [-n] [-r] [-l] [-h] [message]"
+        echo "Usage: fishsay [-f <name>] [-W <width>] [-n] [-r] [-t] [-l] [-h] [message]"
         echo ""
         echo "A cowsay-inspired ASCII art speech bubble generator."
         echo ""
         echo "Options:"
         echo "  -f, --file <name>  Use a named creature (default: fish)"
         echo "  -r, --random           Pick a random creature"
+        echo "  -t, --think            Use a thought bubble instead of a speech bubble"
         echo "  -W, --wrap <width>     Wrap message at <width> characters (default: 40)"
         echo "  -n, --nowrap           Disable word wrapping"
         echo "  -l, --list             List available creatures"
@@ -43,8 +44,8 @@ function fishsay
     set -l creatures_dir $data_dir/fishsay/fish
 
     if set -q _flag_list
-        for f in $creatures_dir/*.fish
-            basename $f .fish
+        for f in $creatures_dir/*.txt
+            basename $f .txt
         end
         return 0
     end
@@ -73,22 +74,15 @@ function fishsay
     # Load creature
     set -l creature_name fish
     if set -q _flag_random
-        set -l all_creatures $creatures_dir/*.fish
-        set creature_name (basename (echo $all_creatures[(random 1 (count $all_creatures))]) .fish)
+        set -l all_creatures $creatures_dir/*.txt
+        set creature_name (basename (echo $all_creatures[(random 1 (count $all_creatures))]) .txt)
     else if set -q _flag_file
         set creature_name $_flag_file
     end
 
-    set -l creature_file $creatures_dir/$creature_name.fish
+    set -l creature_file $creatures_dir/$creature_name.txt
     if not test -f $creature_file
         echo "fishsay: creature '$creature_name' not found in $creatures_dir" >&2
-        return 1
-    end
-
-    source $creature_file
-
-    if not set -q the_fish
-        echo "fishsay: '$creature_name.fish' did not define \$the_fish" >&2
         return 1
     end
 
@@ -117,7 +111,9 @@ function fishsay
     for i in (seq $num_lines)
         set -l line $lines[$i]
         set -l padding (string repeat -n (math $max_len - (string length -- $line)) ' ')
-        if test $num_lines -eq 1
+        if set -q _flag_think
+            echo "( $line$padding )"
+        else if test $num_lines -eq 1
             echo "< $line$padding >"
         else if test $i -eq 1
             echo "/ $line$padding \\"
@@ -131,5 +127,9 @@ function fishsay
     echo " $border"
 
     # Render the creature, substituting {thoughts} with the connector
-    echo $the_fish | string replace -a '{thoughts}' "\\"
+    set -l connector "\\"
+    if set -q _flag_think
+        set connector "o"
+    end
+    cat $creature_file | string replace -a '{thoughts}' $connector
 end
