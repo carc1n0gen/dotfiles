12561c216c21462d0e955b449ec8d38150726646
Carson Evans <carsonwevans@gmail.com>
2026-04-27 18:53:45 -0400

    [fish] add fishsay to fish greeting

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -3,10 +3,14 @@
 # cow greeting, otherwise print an ascii fish greeting.
 #
 function fish_greeting
-    if command -q fortune && command -q cowsay
+    if command -q fortune && functions -q fishsay
+        fortune -s | fishsay -r
+    else if command -q fortune && command -q cowsay
         fortune -s | cowsay -r
+    else if functions -q fishsay
+        fishsay -r "Welcome to Fish! The friendly interactive shell."
     else
-        echo ' __________________________________________________
+        echo ' --------------------------------------------------
 < Welcome to Fish! The friendly interactive shell. >
  --------------------------------------------------
     \
